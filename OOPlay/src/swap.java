dcf0c6e6a7c2f6aea1faa9a303bad49987913883
John <cpd015@gmail.com>
2018-01-07 15:00:56 -0600

    added comments to swap.java

--- OOPlay/src/swap.java
+++ OOPlay/src/swap.java
@@ -7,17 +7,17 @@ public class swap {
 		x.swap(y); 
 		System.out.println(x); 
 		System.out.println(y); 
-	}
-} 
+	}// end main
+} //end swap
 class StringObject{ 
 	public String str; 
-	public StringObject(String str){ this.str = str; } 
-	public String toString(){ return str; }
+	public StringObject(String str){ this.str = str; } //end constructor
+	public String toString(){ return str; }//end constructor
 
 	public void swap(StringObject a){ 
 		String t = a.str; 
 		a.str = this.str; 
 		this.str = t; 
-	} 
+	} //end constructor
 	
 }//end swap
