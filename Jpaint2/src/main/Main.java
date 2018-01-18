ea41ba6592dacaa919882ad722a456c32d797c12
John <cpd015@gmail.com>
2018-01-18 13:55:40 -0600

    comments added to main

--- Jpaint2/src/main/Main.java
+++ Jpaint2/src/main/Main.java
@@ -1,9 +1,13 @@
 package main;
+import javax.swing.JButton;
+
 import controller.IJPaintController;
 import controller.JPaintController;
 import model.DialogProvider;
 import model.IDialogProvider;
+import model.ShapeColor;
 import model.ApplicationState;
+import view.EventName;
 import view.gui.Gui;
 import view.gui.GuiWindow;
 import view.gui.PaintCanvas;
@@ -11,11 +15,15 @@ import view.interfaces.IGuiWindow;
 import view.interfaces.IUiModule;
 
 public class Main {
-	public static void main(String[] args){
-        IGuiWindow guiWindow = new GuiWindow(new PaintCanvas());
-        IUiModule uiModule = new Gui(guiWindow);
-        ApplicationState appState = new ApplicationState(uiModule);
-        IJPaintController controller = new JPaintController(uiModule, appState);
+	public static void main(String[] args){						//gui window set up the JButton getButton(EventName eventName
+        IGuiWindow guiWindow = new GuiWindow(new PaintCanvas());  //paintcanvas sets up the graphics2d
+        
+        IUiModule uiModule = new Gui(guiWindow);  //gui takes the getDialogResponse and addevent
+        
+        ApplicationState appState = new ApplicationState(uiModule);//gets and sets the responses ala colors
+        
+        IJPaintController controller = new JPaintController(uiModule, appState);  //sets up events ala  activePrimaryColor = ShapeColor.BLUE;
         controller.setup();
     }
+	//test of git
 }
