0c7ce4ec1df03244d1b6a3a1d650c703b90d6b29
John <cpd015@gmail.com>
2018-01-21 18:24:36 -0600

    added elipse and triangle

---
+++ Jpaint2/src/shapes/Elipse.java
@@ -0,0 +1,19 @@
+package shapes;
+
+import java.awt.Point;
+
+public class Elipse implements IShape {
+
+	@Override
+	public Point getOriginPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setOriginPostion(Point p) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
